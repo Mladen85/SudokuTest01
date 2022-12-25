5c660322e3e2e0236e6867b0f9c531c905116bc4
Mladen85 <58631567+Mladen85@users.noreply.github.com>
2022-12-25 04:45:21 +0100

    Initial updates to sudoku_File.c
    Decided to use new function `getLine` in sudoku_File.c

--- user_app/sudoku_File.c
+++ user_app/sudoku_File.c
@@ -1,5 +1,6 @@
+#define _GNU_SOURCE
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 #include "sudoku_defs.h"
 #include "sudoku_File.h"
 #include "sudoku_List.h"
@@ -39,6 +40,69 @@
 //static List_Element_t *head = NULL;
 
 //static List_Element_t *p_st_Element = NULL;
+
+static ssize_t GetLine(char **linePtr, size_t *n, FILE *stream);
+
+static ssize_t GetLine(char **linePtr, size_t *n, FILE *stream)
+{
+	char c;
+	ssize_t read = 0;
+
+	if(*n == 0)
+	{
+		*n = SUDOKU_NUM;
+		*linePtr = (char*)malloc(*n * sizeof(char));
+	}
+	else if(*n < 0)
+	{
+		return -1;
+	}
+	else  // *n is positive number, but linePtr is NULL, return error
+	{
+		// if(!(*linePtr))
+		// {
+		// 	return -1;
+		// }
+	}
+
+	if(*linePtr == NULL)
+	{
+		return -1;
+	}
+
+	printf("GetLine pre-while loop, *n = %d\n(file %s, line %d)\n", *n, __FILE__, __LINE__);
+	c = fgetc(stream);
+	printf("GetLine pre-while loop, c = %d\n\n", c);
+	while (*linePtr && ( c != '\n' ) && ( c != EOF ))
+	{
+		printf("GetLine while loop, read = %d\n(file %s, line %d)\n", read, __FILE__, __LINE__);
+		(*linePtr)[read] = c;
+		read++;
+
+		if(read > *n)
+		{
+			*n = (read * sizeof(char)) + (10 * sizeof(char));
+			*linePtr = (char *)realloc(*linePtr, *n);
+		}
+
+		c = fgetc(stream);
+		printf("GetLine while loop, c = %d\n", c);
+	}
+
+	if(read == 0)
+	{
+		*linePtr == NULL;
+		return -1;
+	}
+	else if(read < *n)
+	{
+		*n = read * sizeof(char);
+		*linePtr = (char *)realloc(*linePtr, *n);
+	}
+
+	printf("return %d\n\n", read);
+	return read;
+}
 
 void File_v_SafeDatOpen(FILE **pFile_sudokuFile, char *pstr_dat_name, char *pstr_dat_mode)
 {
@@ -206,8 +270,11 @@ void File_v_DatToList(FILE *pFile_sudokuFile, List_Element_t **head)
 	List_Element_t *p_st_Element = NULL;
 	// uint8 **ui8_sudTable;
 	uint32 ui32_Id = 0;
-	char c;
-	uint32 i = 0, j = 0;
+	char c, *line = NULL;
+	size_t len = SUDOKU_NUM * sizeof(char);
+	ssize_t read;
+	uint32 lineRead = 0, sudokuRow = 0, j = 0;
+	boolean sudokuFound = FALSE;
 
 	/*
 	//Initialize sudokuTables
@@ -220,31 +287,98 @@ void File_v_DatToList(FILE *pFile_sudokuFile, List_Element_t **head)
 
 	if (pFile_sudokuFile)
 	{
-		printf("Start reading file!\n");
-		while ((c = fgetc(pFile_sudokuFile)) != EOF)
+		printf("Start reading file!\n(file %s, line %d)\n\n", __FILE__, __LINE__);
+		#if 0
+		// while ((c = fgetc(pFile_sudokuFile)) != EOF)
+		// {
+		// 	printf("Char c = %d read!\n(file %s, line %d)\n", c, __FILE__, __LINE__);
+		// 	p_st_Element = NULL;
+		// 	p_st_Element = (List_Element_t *)malloc(sizeof(List_Element_t));
+
+		// 	//Initialize sudokuTables
+		// 	// p_st_Element->sudokuTable = calloc(SUDOKU_NUM, sizeof(uint8 *));
+		// 	// for (i = 0; i < SUDOKU_NUM; i++)
+		// 	// {
+		// 	// 	p_st_Element->sudokuTable[i] = calloc(SUDOKU_NUM, sizeof(uint8));
+		// 	// }
+
+		// 	//Read sudoku table from dat
+		// 	for (i = 0; i < SUDOKU_NUM; i++)
+		// 	{
+		// 		j = 0;
+		// 		printf("Print while loop, i = %d\n(file %s, line %d)\n", i, __FILE__, __LINE__);
+		// 		//while ((c = fgetc(pFile_sudokuFile)) != '\n')
+		// 		while( (c != '\n') && (c != 'E'))
+		// 		{
+		// 			c = fgetc(pFile_sudokuFile);
+		// 			p_st_Element->sudokuTable[i][j] = c - 48; // Subtract 48 from ASCII (ASCII 49 - dec 1)
+		// 			j++;
+		// 			printf("j = %d\nchar = %c -> %d\n", j, c, c);
+		// 		}
+		// 		printf("Exit while loop\n(file %s, line %d)\n", __FILE__, __LINE__);
+		// 	}
+		// 	printf("Exit for loop\n(file %s, line %d)\n", __FILE__, __LINE__);
+
+		// 	p_st_Element->tableID = ui32_Id;
+		// 	p_st_Element->next = NULL;
+
+		// 	ui32_Id++;
+		// 	printf("%d times\n", ui32_Id);
+
+		// 	if (*head == NULL)
+		// 	{
+		// 		*head = p_st_Element;
+		// 	}
+		// 	else
+		// 	{
+		// 		List_v_AddToEndOfList(*head, p_st_Element);
+		// 	}
+		// }
+		#endif /* #if 0 */
+
+		line = (char *)malloc(len);
+		while ((read = GetLine(&line, &len, pFile_sudokuFile)) != -1)
 		{
-			printf("Char c = %d read! ",c);
 			p_st_Element = NULL;
 			p_st_Element = (List_Element_t *)malloc(sizeof(List_Element_t));
-
-			//Initialize sudokuTables
-			// p_st_Element->sudokuTable = calloc(SUDOKU_NUM, sizeof(uint8 *));
-			// for (i = 0; i < SUDOKU_NUM; i++)
-			// {
-			// 	p_st_Element->sudokuTable[i] = calloc(SUDOKU_NUM, sizeof(uint8));
-			// }
-
-			//Read sudoku table from dat
-			for (i = 0; i < SUDOKU_NUM; i++)
+			printf("sizeof(*p_st_Element = %u\n", sizeof(*p_st_Element));
+			if(read < SUDOKU_NUM)
 			{
-				j = 0;
-				//while ((c = fgetc(pFile_sudokuFile)) != '\n')
-				while( (c != '\n') && (c != 'E'))
+				if (line[0] != 'S')
 				{
-					c = fgetc(pFile_sudokuFile);
-					p_st_Element->sudokuTable[i][j] = c - 48; // Subtract 48 from ASCII (ASCII 49 - dec 1)
-					j++;
+					break;
 				}
+				else
+				{
+					len = SUDOKU_NUM * sizeof(char);
+					lineRead++;
+					continue;
+				}
+			}
+			else if(read == SUDOKU_NUM)
+			{
+				while(sudokuRow != (SUDOKU_NUM - 1))
+				{
+					sudokuRow = lineRead % 10 - 1;
+					printf("File_v_DatToList:\n");
+					printf("lineRead = %d\nsudokuRow = %d\n", lineRead, sudokuRow);
+					if((sudokuRow < 0) || (sudokuRow >= SUDOKU_NUM))
+					{
+						break;
+					}
+
+					for (j = 0; j < SUDOKU_NUM; j++)
+					{
+						printf("line[j] = %d\n", line[j]);
+						p_st_Element->sudokuTable[sudokuRow][j] = line[j] - 48;
+					}
+					printf("j = %d\nchar = %c -> %d\n", j, line[j], line[j]);
+					lineRead++;
+				}
+			}
+			else
+			{
+				break;
 			}
 
 			p_st_Element->tableID = ui32_Id;
